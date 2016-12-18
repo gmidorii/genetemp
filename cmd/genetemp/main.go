6a0a041f49e80248818cae5d5ed14f80fa4f0bcf
tagasouichiro <stitow14@gmail.com>
2016-12-18 20:31:10 +0900

    add write file

--- cmd/genetemp/main.go
+++ cmd/genetemp/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"log"
 	"gopkg.in/yaml.v2"
@@ -20,5 +19,9 @@ func main()  {
 
 	var class Class
 	err = yaml.Unmarshal(buf, &class)
-	fmt.Println(class.Name)
+
+	err = ioutil.WriteFile("output/" + class.Name, []byte(class.Name), 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
